6002aef8bc0014c7cbd220971bfddc9cbc9c27d0
gorandalf <gstevanovic@outlook.com>
2016-12-14 14:35:10 +0100

    Update Script.PreDeployment.sql

--- DatevDemo/Scripts/Script.PreDeployment.sql
+++ DatevDemo/Scripts/Script.PreDeployment.sql
@@ -1,4 +1,9 @@
-﻿/*
+
+
+/*
+ HALLO
+*/
+*/*
  Pre-Deployment Script Template							
 --------------------------------------------------------------------------------------
  This file contains SQL statements that will be executed before the build script.	
